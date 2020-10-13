8c2a4a28240b7e387f721d0ef83b3580ae47f0c6
Bob <Bob@itcast.cn>
2020-10-13 18:27:22 +0800

    web配置类

---
+++ src/main/java/com/alligator/realwar/config/WebConfig.java
@@ -0,0 +1,48 @@
+package com.alligator.realwar.config;
+
+import com.alligator.realwar.interceptor.RateLimitInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.omg.CORBA.PRIVATE_MEMBER;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * web配置类
+ */
+@Configuration
+@EnableWebMvc
+@Slf4j
+public class WebConfig implements WebMvcConfigurer {
+
+    @Resource
+    private RateLimitInterceptor rateLimitInterceptor;
+
+
+    /**
+     * 向web中添加拦截器的方法
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        //注册拦截器
+        registry.addInterceptor(rateLimitInterceptor)
+                .addPathPatterns("/api/**");
+
+    }
+
+    /**
+     * 静态资源配置（这里配置的路径不会经过拦截器)
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
+
+}
